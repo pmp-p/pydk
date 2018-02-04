aafb8f4a162c7afc9b8c7a978dbcd3729a4e51cf
pmp-p <pmpp.pub@gmail.com>
2018-02-04 22:27:58 +0100

    hacked pymain

---
+++ sources.32/cpython/cpython_main/interpreter37m.cpp
@@ -0,0 +1,18 @@
+#define PY_PREPARE 0
+#define PY_INTERPRETER 1
+#define PY_REPL 1
+
+static char *root_folder = "/data/data/u.r";
+
+#define PY_PATH "/data/data/u.r/usr/lib/python3.7"
+#define PYTHONPATH "/data/data/u.r/usr/lib/python3.7/lib-dynload:/data/data/u.r/usr/lib/python3.7:/data/data/u.r/usr/lib/python3"
+#define LIB_PYTHON "libpython3.7m.so"
+#define PY_LIB "/data/data/u.r/usr/lib/python3.7"
+#define PY_LIBS "/data/data/u.r/usr/lib/python3.7:/data/data/u.r/usr/lib/python3"
+
+#define PYVER 37
+
+#define PY_LOG "Python3.7m"
+
+#include "cpython3xm.cpp"
+
