87853b751dab0d78f190a414dfac447162006c07
pmp-p <mail.peny@free.fr>
2020-10-04 18:54:59 +0200

    system packages for debian based

--- docs/ubuntu20.sh
+++ docs/ubuntu20.sh
@@ -5,5 +5,9 @@ echo NDK https://developer.android.com/ndk/downloads
 sudo apt install \
  build-essential clang git\
  python3-pip python3-venv\
- cmake ninja-build
+ cmake ninja-build\
+ libfuse-dev\
+ libsqlite3-dev ninja-build bison flex zlib1g-dev\
+ libgl1-mesa-dev libgles2-mesa-dev libglapi-mesa mesa-common-dev libegl1-mesa-dev\
+ libbullet-dev libfreetype6-dev libjpeg-dev libode-dev libopenal-dev libpng-dev libssl-dev libogg-dev libvorbis-dev
 
